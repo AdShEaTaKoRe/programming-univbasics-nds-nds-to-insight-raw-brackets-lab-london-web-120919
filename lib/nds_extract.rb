fec5ab2620206b3f1a188d6e436f31b363d938ce
Tommy Berman <tomy.berman1@gmail.com>
2019-12-04 21:42:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,6 +3,7 @@ require 'directors_database'
 require 'pp'
 def directors_totals(nds)
 
+<<<<<<< HEAD
     directors = 0
     result = {}
     while directors < nds.length do
@@ -17,4 +18,7 @@ def directors_totals(nds)
         directors += 1  
         end
     result
+=======
+
+>>>>>>> 2ab89ea85ae21d6d65b2470f4c84b9c3da354d43
 end
